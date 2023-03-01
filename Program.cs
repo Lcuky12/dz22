44a77b5bb934782159a919ca624104611562e247
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-01 13:02:42 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5;i<97; i+=7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
